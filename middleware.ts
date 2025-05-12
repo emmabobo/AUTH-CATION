5516a8b3139f0db1682b235b3cb08ebc5471f69c
emmanbobo <olawalealawode@gmail.com>
2025-05-12 17:16:44 +0100

    middleware fixed

--- middleware.ts
+++ middleware.ts
@@ -1,3 +1,18 @@
-export const runtime = "nodejs";
+import { NextRequest ,NextResponse } from "next/server";
 
-export { auth as middleware } from "@/auth"
+
+export function middleware(request: NextRequest) {
+    const token = request.cookies.get("next-auth.session-token");
+
+  // If there's no token, redirect to login
+  if (!token) {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+
+  return NextResponse.next();
+}
+
+// Optional: specify which routes should use this middleware
+export const config = {
+  matcher: ["/dashboard/:path*", "/profile/:path*"], // Update these paths as needed
+};
